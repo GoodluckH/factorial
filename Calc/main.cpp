2305e8cb63b4d3a9dbb07ea9c5613f4d0b2a6e73
GoodluckH <lixipu98@gmail.com>
2017-03-11 15:51:56 -0800

    Initial Commit

---
+++ Calc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calc
+//
+//  Created by Robin on 11/03/2017.
+//  Copyright © 2017 XIPU.Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
