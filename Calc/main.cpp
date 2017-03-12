24dc16ccc0b418546ca37339186d2f81e05c8991
GoodluckH <lixipu98@gmail.com>
2017-03-11 20:11:36 -0800

    q

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -9,56 +9,12 @@
 #include <iostream>
 #include<cstring>
 
-//using namespace std;
+using namespace std;
 
 
-int max = 5000;
-
-void display(int arr[]){
-    int ctr = 0;
-    for (int i=0; i<max; i++){
-        if (!ctr && arr[i])         ctr = 1;
-        if(ctr)
-            
-            std::cout<<arr[i]<<std::scientific;
-    }
-}
 
 
-void factorial(int arr[], int n){
-    if (!n) return;
-    int carry = 0;
-    for (int i=max-1; i>=0; --i){
-        arr[i] = (arr[i] * n) + carry;
-        carry = arr[i]/10;
-        arr[i] %= 10;
-    }
-    factorial(arr,n-1);
-}
-
-int main(){
-    int *arr = new int[max];
-    std::memset(arr,0,max*sizeof(int));
-    arr[max-1] = 1;
-    int num;
-    std::cout<<"Enter the number: ";
-    std::cin>>num;
-    std::cout<<"factorial of "<<num<<" is :\n"<<std::scientific;
-    factorial(arr,num);
-    display(arr);
-    delete[] arr;
-    return 0;
-}/*
- long long int factorial(int N)
-{
-    long long ans = 1;
-    int i;
-    for(i=1; i <= N; i++)
-        ans *= i;
-    return ans;
-}
-
-
+ 
 int main(int argc, const char * argv[]) {
     double n;                   //declare variables
     double f;
@@ -66,7 +22,7 @@ int main(int argc, const char * argv[]) {
     double *p = new double;     //Create pointer p and allocate it to the heap
     p=&f;                       /* assign value of f to pointer p*/
     
-/*
+
     cout << "Your number: ";
     cin>>n;
     if (n<0){
@@ -98,4 +54,4 @@ int main(int argc, const char * argv[]) {
     
     
     return 0;
-} */
+}
